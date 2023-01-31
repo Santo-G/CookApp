af38d2d4dbf1f0c63c463d9b0178a5b413cd36b8
Santo <77076220+Santo-G@users.noreply.github.com>
2023-01-31 08:36:10 +0100

    feat: adding a Composable inside the xml layout

--- app/src/main/java/com/santog/cookapp/RecipeListFragment.kt
+++ app/src/main/java/com/santog/cookapp/RecipeListFragment.kt
@@ -1,22 +1,57 @@
 package com.santog.cookapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
 
 class RecipeListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        /* // Using Jetpack Compose inside this Fragment...
         return ComposeView(requireContext()).apply {
             setContent {
                 Text("Recipe List Fragment")
             }
         }
+        */
+
+        // use HorizontalDottedProgressBar (without a xml layout) inside this Fragment
+        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+            // Add here a Composable inside the xml layout
+            Column (
+                modifier = Modifier
+                    .border(border = BorderStroke(1.dp, Color.Black))
+                    .padding(16.dp)
+            ){
+                Text("This is a Composable inside the Fragment")
+                Spacer(modifier = Modifier.padding(10.dp))
+                CircularProgressIndicator()
+                Spacer(modifier = Modifier.padding(10.dp))
+                Text("NEAT")
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                val customView = HorizontalDottedProgress(context)
+                AndroidView( factory = {customView} )
+            }
+        }
+        return view
     }
 
 }
