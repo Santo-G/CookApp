0188dadd0c980cd26ab351d91683344e8059be55
Santo <77076220+Santo-G@users.noreply.github.com>
2023-02-19 18:10:41 +0100

    feat: added retrofit and Gson parser + recipe DTO

---
+++ app/src/main/java/com/santog/cookapp/network/model/RecipeDTO.kt
@@ -0,0 +1,35 @@
+package com.santog.cookapp.network.model
+
+
+import com.google.gson.annotations.SerializedName
+import androidx.annotation.Keep
+
+@Keep
+data class RecipeDTO(
+    @SerializedName("cooking_instructions")
+    val cookingInstructions: Any,
+    @SerializedName("date_added")
+    val dateAdded: String,
+    @SerializedName("date_updated")
+    val dateUpdated: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("featured_image")
+    val featuredImage: String,
+    @SerializedName("ingredients")
+    val ingredients: List<String>,
+    @SerializedName("long_date_added")
+    val longDateAdded: Int,
+    @SerializedName("long_date_updated")
+    val longDateUpdated: Int,
+    @SerializedName("pk")
+    val pk: Int,
+    @SerializedName("publisher")
+    val publisher: String,
+    @SerializedName("rating")
+    val rating: Int,
+    @SerializedName("source_url")
+    val sourceUrl: String,
+    @SerializedName("title")
+    val title: String
+)
