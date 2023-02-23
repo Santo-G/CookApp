622c1b5422def1e9e880fc8ac99a439d3da5b23b
Santo <77076220+Santo-G@users.noreply.github.com>
2023-02-23 08:02:47 +0100

    feat: added viewModel dependency and Hilt module

--- app/src/main/java/com/santog/cookapp/presentation/ui/RecipeViewModel.kt
+++ app/src/main/java/com/santog/cookapp/presentation/ui/RecipeViewModel.kt
@@ -1,10 +1,13 @@
 package com.santog.cookapp.presentation.ui
 
 import androidx.lifecycle.ViewModel
+import com.santog.cookapp.network.model.RecipeDTOMapper
 import javax.inject.Inject
 
 class RecipeViewModel @Inject constructor(
 
 ) : ViewModel() {
+
+    // TODO empty ViewModel
 
 }
